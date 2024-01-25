827d6e4eb60837b79a97d9fa75b8c531bf99f51a
Sasha Shut <sanyashut159@gmail.com>
2024-01-25 04:35:55 +0200

    added main basic set up styles and fonts

--- client/src/pages/home/ui/page.tsx
+++ client/src/pages/home/ui/page.tsx
@@ -2,5 +2,17 @@ import { FC } from "react";
 // import styles from "./styles.module.scss";
 
 export const HomePage: FC = () => {
-	return <div>page</div>;
+	return (
+		<div>
+			<h2>tests</h2>
+			<p>test text Anime Manga</p>
+			<p>test list</p>
+			<ul>
+				<li>sфів</li>
+				<li>1</li>
+				<li>1</li>
+				<li>1</li>
+			</ul>
+		</div>
+	);
 };
