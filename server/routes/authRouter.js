eef1732b4daaf557951a6ebedb9da9e374a0e92d
Sasha Shut <sanyashut159@gmail.com>
2024-01-27 17:56:25 +0200

    created models and registration controller

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,8 +1,19 @@
 const Router = require("express");
 const router = new Router();
 const authController = require("../controllers/authController");
+const { check } = require("express-validator");
 
-router.post("/registration", authController.registration);
+router.post(
+	"/registration",
+	[
+		check("username", "Имя пользователя не может быть пустым").notEmpty(),
+		check(
+			"password",
+			"Пароль должен быть больше 8 и меньше 20 символов"
+		).isLength({ min: 8, max: 20 }),
+	],
+	authController.registration
+);
 router.post("/login", authController.login);
 router.get("/users", authController.getUsers);
 
