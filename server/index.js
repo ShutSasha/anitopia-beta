6efe6166c6328857716a0a2a0696302d1448e88f
Artem <temaprgod8@gmail.com>
2024-01-30 20:21:34 +0200

    cors update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+	credentials:true,
+	origin: process.env.CLIENT_URL
+}));
 app.use("/api", router);
 app.use(errorMiddleware);
 
