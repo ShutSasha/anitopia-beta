51bd92816865fd68d6427dd70b333718f35efe86
Artem <temaprgod8@gmail.com>
2024-02-01 14:14:57 +0200

    [server] edit user avatar upload method

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -4,39 +4,15 @@ const userService = require("../services/UserService");
 const ApiError = require("../errors/apiError");
 const multer = require("multer");
 const upload = multer({ dest: "server/uploads/" });
-const ImageKit = require("imagekit");
-const fs = require('fs');
-const uuid = require('uuid')
-
-var imagekit = new ImageKit({
-	publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
-	privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
-	urlEndpoint: process.env.IMAGE_KIT_URL_ENDPOINT,
-});
 
 class profileController {
 	async uploadAvatarUser(req, res, next) {
 		try {
-			var fileName = uuid.v4() + "jpg";
+			const{username} = req.body;
 			const uploadedFile = req.file.path;// Используйте req.file вместо req.files.img
-			const fileStream = fs.createReadStream(uploadedFile);
+			const result = await userService.changeUserIcon(uploadedFile,username)
+			return res.json(result);
 
-			console.log(uploadedFile.name);
-			imagekit.upload({
-				file : fileStream, //required
-				fileName : fileName,   //required
-				folder: 'user_icons',
-				extensions: [
-				{
-					name: "google-auto-tagging",
-					maxTags: 5,
-					minConfidence: 95
-				}
-			]
-		}, function(error, result) {
-				if(error) console.log(error);
-				else console.log(result);
-			});
 		} catch (e) {
 			console.error(e);
 			next(e); // Передайте ошибку в обработчик ошибок Express
