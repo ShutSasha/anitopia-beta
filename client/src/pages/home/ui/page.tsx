e520c2370b3db0b5ef7b04747e018a9ad30cdb8a
Sasha Shut <sanyashut159@gmail.com>
2024-02-13 18:02:35 +0200

    set up prettier config

--- client/src/pages/home/ui/page.tsx
+++ client/src/pages/home/ui/page.tsx
@@ -1,117 +1,117 @@
-import { FC, useContext, useEffect, useState } from "react";
-import styles_h from "./styles.module.scss";
-import { Header } from "../../../widgets/header";
-import { Context } from "../../../main";
-import { observer } from "mobx-react-lite";
-import { Loader } from "../../../shared";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import "react-multi-carousel/lib/styles.css";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css"; // Default theme
+import { FC, useContext, useEffect, useState } from 'react'
+import styles_h from './styles.module.scss'
+import { Header } from '../../../widgets/header'
+import { Context } from '../../../main'
+import { observer } from 'mobx-react-lite'
+import { Loader } from '../../../shared'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import 'react-multi-carousel/lib/styles.css'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css' // Default theme
 
 export const HomePage: FC = observer(() => {
-	const { store } = useContext(Context);
-	const [searchText, setSearchText] = useState<string>("");
-	const [animeSeasonData, setAnimeSeasonData] = useState([]);
+   const { store } = useContext(Context)
+   const [searchText, setSearchText] = useState<string>('')
+   const [animeSeasonData, setAnimeSeasonData] = useState([])
 
-	const headers = {
-		Authorization: `Bearer ${localStorage.getItem(`token`)}`,
-	};
+   const headers = {
+      Authorization: `Bearer ${localStorage.getItem(`token`)}`,
+   }
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const res = await axios.get(
-					"http://localhost:5000/api/anime/season-anime"
-				);
-				setAnimeSeasonData(res.data);
-			} catch (err) {
-				console.error(err);
-			}
-		};
-		fetchData();
-	}, []);
+   useEffect(() => {
+      const fetchData = async () => {
+         try {
+            const res = await axios.get(
+               'http://localhost:5000/api/anime/season-anime',
+            )
+            setAnimeSeasonData(res.data)
+         } catch (err) {
+            console.error(err)
+         }
+      }
+      fetchData()
+   }, [])
 
-	const getUsersClick = async () => {
-		const response = await axios.get("http://localhost:5000/api/auth/users", {
-			headers,
-		});
-		console.log(response);
-	};
+   const getUsersClick = async () => {
+      const response = await axios.get('http://localhost:5000/api/auth/users', {
+         headers,
+      })
+      console.log(response)
+   }
 
-	if (store.isLoading) {
-		return <Loader />;
-	}
+   if (store.isLoading) {
+      return <Loader />
+   }
 
-	return (
-		<>
-			<Header />
-			<div className={styles_h.wrapper}>
-				<div className={styles_h.container}>
-					<div className={styles_h.search_filter_line}>
-						<div className={styles_h.search}>
-							<input
-								className={styles_h.search_input}
-								placeholder="НАЙТИ АНИМЕ ПО НАЗВАНИЮ"
-								value={searchText}
-								onChange={(e) => setSearchText(e.target.value)}
-								type="text"
-							/>
-						</div>
-						<div className={styles_h.filter}>
-							<button className={styles_h.filter_btn}>
-								<span className={styles_h.filter_icon}></span>
-								РАСКРЫТЬ ФИЛЬТР
-							</button>
-						</div>
-					</div>
-					<div className={styles_h.cards_anime_container}>
-						<div className={styles_h.anime_season_block}>
-							<h2 className={styles_h.anime_season_title}>
-								Аниме зимнего сезона
-							</h2>
-						</div>
-						<div>
-							<Splide
-								options={{
-									type: "loop",
-									perMove: 2,
-									perPage: 6,
-									pagination: false,
-								}}
-							>
-								{animeSeasonData &&
-									animeSeasonData
-										.slice(0, 12)
-										.map((card: any, index: number) => (
-											<SplideSlide key={index}>
-												<Link
-													to={`anime/${card.id}`}
-													className={styles_h.card}
-												>
-													<div
-														style={{
-															backgroundImage: `url(${card.material_data.poster_url})`,
-														}}
-														className={styles_h.card_background}
-													></div>
-												</Link>
-												<div
-													title={card.title}
-													className={styles_h.card_text_block}
-												>
-													{card.title}
-												</div>
-											</SplideSlide>
-										))}
-							</Splide>
-						</div>
-						{store.isAuth && <div>-_----_----_-----_---</div>}
-						<button onClick={() => getUsersClick()}>get users</button>
-					</div>
-				</div>
-			</div>
-		</>
-	);
-});
+   return (
+      <>
+         <Header />
+         <div className={styles_h.wrapper}>
+            <div className={styles_h.container}>
+               <div className={styles_h.search_filter_line}>
+                  <div className={styles_h.search}>
+                     <input
+                        className={styles_h.search_input}
+                        placeholder='НАЙТИ АНИМЕ ПО НАЗВАНИЮ'
+                        value={searchText}
+                        onChange={(e) => setSearchText(e.target.value)}
+                        type='text'
+                     />
+                  </div>
+                  <div className={styles_h.filter}>
+                     <button className={styles_h.filter_btn}>
+                        <span className={styles_h.filter_icon}></span>
+                        РАСКРЫТЬ ФИЛЬТР
+                     </button>
+                  </div>
+               </div>
+               <div className={styles_h.cards_anime_container}>
+                  <div className={styles_h.anime_season_block}>
+                     <h2 className={styles_h.anime_season_title}>
+                        Аниме зимнего сезона
+                     </h2>
+                  </div>
+                  <div>
+                     <Splide
+                        options={{
+                           type: 'loop',
+                           perMove: 2,
+                           perPage: 6,
+                           pagination: false,
+                        }}
+                     >
+                        {animeSeasonData &&
+                           animeSeasonData
+                              .slice(0, 12)
+                              .map((card: any, index: number) => (
+                                 <SplideSlide key={index}>
+                                    <Link
+                                       to={`anime/${card.id}`}
+                                       className={styles_h.card}
+                                    >
+                                       <div
+                                          style={{
+                                             backgroundImage: `url(${card.material_data.poster_url})`,
+                                          }}
+                                          className={styles_h.card_background}
+                                       ></div>
+                                    </Link>
+                                    <div
+                                       title={card.title}
+                                       className={styles_h.card_text_block}
+                                    >
+                                       {card.title}
+                                    </div>
+                                 </SplideSlide>
+                              ))}
+                     </Splide>
+                  </div>
+                  {store.isAuth && <div>-_----_----_-----_---</div>}
+                  <button onClick={() => getUsersClick()}>get users</button>
+               </div>
+            </div>
+         </div>
+      </>
+   )
+})
--- client/src/shared/ui/ErrorBoundary/index.tsx
+++ client/src/shared/ui/ErrorBoundary/index.tsx
@@ -1,32 +1,32 @@
-import { ReactNode, useState, useEffect, FC, useContext } from "react";
-import { Toast } from "../..";
-import { Context } from "../../../main";
+import { ReactNode, useState, useEffect, FC, useContext } from 'react';
+import { Toast } from '../..';
+import { Context } from '../../../main';
 
 export const ErrorBoundary: FC<{ children: ReactNode }> = ({ children }) => {
-	const { store } = useContext(Context);
-	const [error, setError] = useState<Error | null>(null);
-	const [, setShowToast] = useState<boolean>();
+  const { store } = useContext(Context);
+  const [error, setError] = useState<Error | null>(null);
+  const [, setShowToast] = useState<boolean>();
 
-	useEffect(() => {
-		const errorHandler = (event: ErrorEvent) => {
-			setError(event.error);
-		};
-		window.addEventListener("error", errorHandler);
-		return () => {
-			window.removeEventListener("error", errorHandler);
-		};
-	}, []);
+  useEffect(() => {
+    const errorHandler = (event: ErrorEvent) => {
+      setError(event.error);
+    };
+    window.addEventListener('error', errorHandler);
+    return () => {
+      window.removeEventListener('error', errorHandler);
+    };
+  }, []);
 
-	return (
-		<div>
-			{children}
-			{error && (
-				<Toast
-					clearIsError={() => store.setIsError(false)}
-					onClose={() => setShowToast(false)}
-					message={error.message}
-				/>
-			)}
-		</div>
-	);
+  return (
+    <div>
+      {children}
+      {error && (
+        <Toast
+          clearIsError={() => store.setIsError(false)}
+          onClose={() => setShowToast(false)}
+          message={error.message}
+        />
+      )}
+    </div>
+  );
 };
