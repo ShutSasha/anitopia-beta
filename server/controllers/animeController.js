38ee0ceea3794314fe1cb25b2041dc1232442418
Sasha Shut <sanyashut159@gmail.com>
2024-02-14 16:50:22 +0200

    [server] added method for get all anime

--- server/controllers/animeController.js
+++ server/controllers/animeController.js
@@ -1,89 +1,115 @@
-const animeSerials = require("../anime-serial.json");
-const AnimeService = require("../services/AnimeService");
-const { format } = require("date-fns");
-const { ca } = require("date-fns/locale/ca");
-const { resetWatchers } = require("nodemon/lib/monitor/watch");
+const { default: axios } = require('axios')
+const animeSerials = require('../anime-serial.json')
+const AnimeService = require('../services/AnimeService')
+const { format } = require('date-fns')
+const { ca } = require('date-fns/locale/ca')
+const { resetWatchers } = require('nodemon/lib/monitor/watch')
 
 class AnimeController {
-	async getAnimeList(req, res, next) {
-		try {
-			const data = animeSerials;
-			const uniqueData = await AnimeService.removeDuplicates(data, "title");
-			let sortedData = AnimeService.sortByRating(uniqueData);
-			const query = req.query.search
-			console.log(query);
+   async getAnimeList(req, res, next) {
+      try {
+         const data = animeSerials
+         const uniqueData = await AnimeService.removeDuplicates(data, 'title')
+         let sortedData = AnimeService.sortByRating(uniqueData)
+         const query = req.query.search
+         console.log(query)
 
-			if(query){
-				sortedData = await AnimeService.findAnime(sortedData,query);
-			}
+         if (query) {
+            sortedData = await AnimeService.findAnime(sortedData, query)
+         }
 
-			const startIndex = req.query.page * req.query.limit || 0;
-			const count = req.query.limit || 10;
-			const result = await AnimeService.getAnimeSubset(
-				sortedData,
-				startIndex,
-				count
-			);
-			return res.json({
-				data: result,
-				length: sortedData.length
-			});
-		} catch (e) {
-			next(e);
-		}
-	}
+         const startIndex = req.query.page * req.query.limit || 0
+         const count = req.query.limit || 10
+         const result = await AnimeService.getAnimeSubset(
+            sortedData,
+            startIndex,
+            count,
+         )
+         return res.json({
+            data: result,
+            length: sortedData.length,
+         })
+      } catch (e) {
+         next(e)
+      }
+   }
 
-	async getAnimeSeason(req, res, next) {
-		try {
-			// const date = new Date();
-			//const formattedDate = format(date, "yyyy-MM-dd");
+   async getAnimeSeason(req, res, next) {
+      try {
+         // const date = new Date();
+         //const formattedDate = format(date, "yyyy-MM-dd");
 
-			const animeData = animeSerials;
-			const sliceData = animeData.slice(0, 100);
-			const filterData = await AnimeService.removeDuplicates(
-				sliceData,
-				"title"
-			);
+         const animeData = animeSerials
+         const sliceData = animeData.slice(0, 100)
+         const filterData = await AnimeService.removeDuplicates(
+            sliceData,
+            'title',
+         )
 
-			const animeWithDate = filterData.filter(
-				(item) => item.material_data.premiere_world != undefined
-			);
+         const animeWithDate = filterData.filter(
+            (item) => item.material_data.premiere_world != undefined,
+         )
 
-			const seasonAnime = animeWithDate.filter((item) => {
-				const premiereYear = Number(
-					item.material_data.premiere_world.split("-")[0]
-				);
-				return premiereYear === 2023;
-			});
+         const seasonAnime = animeWithDate.filter((item) => {
+            const premiereYear = Number(
+               item.material_data.premiere_world.split('-')[0],
+            )
+            return premiereYear === 2023
+         })
 
-			return res.json(seasonAnime);
-		} catch (error) {
-		}
-	}
+         return res.json(seasonAnime)
+      } catch (error) {}
+   }
 
-	async getAnime(req, res, next) {
-		try {
-			const { id } = req.params;
-			const animeData = animeSerials;
-			const anime = animeData.find((item) => item.id === id);
+   async getAnime(req, res, next) {
+      try {
+         const { id } = req.params
+         const animeData = animeSerials
+         const anime = animeData.find((item) => item.id === id)
 
-			return res.json(anime);
-		} catch (error) {
-		}
-	}
+         return res.json(anime)
+      } catch (error) {}
+   }
 
-	async searchAnime(req, res, next) {
-		try {
-			const { title } = req.params;
-			const data = animeSerials;
-			const searchedAnime = await AnimeService.findAnime(data, title);
-			const uniqueData = await AnimeService.removeDuplicates(searchedAnime);
-			console.log("Длинна списка: " + searchedAnime.length);
-			return res.json(searchedAnime);
-		} catch (e) {
-			next(e);
-		}
-	}
+   async getAllAnime(req, res, next) {
+      try {
+         let newArray = []
+         let nextUrl = `https://kodikapi.com/list?token=${process.env.KODIK_TOKEN}&types=anime-serial&limit=100&with_material_data=true`
+
+         while (nextUrl) {
+            let response = await axios.get(nextUrl)
+            newArray.push(...response.data.results)
+
+            nextUrl = response.data.next_page
+               ? `https://kodikapi.com/list?token=${process.env.KODIK_TOKEN}&types=anime-serial&limit=100&with_material_data=true&next=${response.data.next_page}`
+               : null
+
+            if (nextUrl === null || newArray.length === 200) {
+               break
+            }
+         }
+
+         return res.json(newArray.length)
+      } catch (error) {
+         console.error('Error fetching anime data:', error)
+         return res
+            .status(500)
+            .json({ error: 'An error occurred while fetching anime data' })
+      }
+   }
+
+   async searchAnime(req, res, next) {
+      try {
+         const { title } = req.params
+         const data = animeSerials
+         const searchedAnime = await AnimeService.findAnime(data, title)
+         const uniqueData = await AnimeService.removeDuplicates(searchedAnime)
+         console.log('Длинна списка: ' + searchedAnime.length)
+         return res.json(searchedAnime)
+      } catch (e) {
+         next(e)
+      }
+   }
 }
 
-module.exports = new AnimeController();
+module.exports = new AnimeController()
--- server/routes/animeRouter.js
+++ server/routes/animeRouter.js
@@ -1,9 +1,11 @@
-const Router = require("express");
-const router = new Router();
-const animeController = require("../controllers/animeController");
+const Router = require('express')
+const router = new Router()
+const animeController = require('../controllers/animeController')
 
-router.get("/list-anime", animeController.getAnimeList);
-router.get("/season-anime", animeController.getAnimeSeason);
-router.get("/:id", animeController.getAnime);
-router.get("/search/:title", animeController.searchAnime)
-module.exports = router;
+router.get('/', animeController.getAllAnime)
+router.get('/list-anime', animeController.getAnimeList)
+router.get('/season-anime', animeController.getAnimeSeason)
+router.get('/:id', animeController.getAnime)
+router.get('/search/:title', animeController.searchAnime)
+
+module.exports = router
