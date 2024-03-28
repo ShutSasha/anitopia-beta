c975dd9a1fe6ec29de2cd5820fe1a355041f3d67
Sasha Shut <sanyashut159@gmail.com>
2024-03-28 16:07:47 +0200

    [server] connected swagger to project

---
+++ server/utils/swagger.js
@@ -0,0 +1,30 @@
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const options = {
+   definition: {
+      openapi: '3.0.0',
+      info: {
+         title: 'REST API Docs',
+         description: 'Here we are storing our apis',
+      },
+   },
+   apis: ['./routes/**/*js'],
+}
+
+const swaggerSpec = swaggerJsdoc(options)
+
+function swaggerDocs(app, port) {
+   // Swagger page
+   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+   // Docs in JSON format
+   app.get('/docs.json', (req, res) => {
+      res.setHeader('Content-Type', 'application/json')
+      res.send(swaggerSpec)
+   })
+
+   console.log(`Docs available at http://localhost:${port}/docs`)
+}
+
+module.exports = swaggerDocs
