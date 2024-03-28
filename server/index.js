b506c0e7f16d152265a053cf98d79423b1f9e4f8
Sasha Shut <sanyashut159@gmail.com>
2024-03-28 20:33:13 +0200

    Hotfix v11
    fix dotenv

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+   require('dotenv').config()
+}
 const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
