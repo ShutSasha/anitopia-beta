d7cbc4a4fc281544b75f3471cc330aacb44f57f2
Sasha Shut <sanyashut159@gmail.com>
2024-04-01 21:41:38 +0300

    [client] added dynamic-anime-section

---
+++ client/src/entities/ui/updated-anime-card/updated-anime-card.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+import styles from './styles.module.scss'
+
+export const UpdatedAnimeCard: FC = () => {
+   return (
+      <div className={styles.container}>
+         <div>
+            <div>
+               <img src='' alt='' />
+               <p></p>
+            </div>
+            <div></div>
+         </div>
+      </div>
+   )
+}
