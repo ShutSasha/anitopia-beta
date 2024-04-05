6ece010e823cff6d9362fc609c4fff13db48a51a
Sasha Shut <sanyashut159@gmail.com>
2024-04-05 17:14:04 +0300

    [client] more in decsription
    -remove ErrorBoundary (old version)
    -remove toast(old version)
    -fix handle errors in store
    -add check user agreement in registration page
    - added check for ServermessageError

--- client/src/app/helpers/functions.tsx
+++ client/src/app/helpers/functions.tsx
@@ -18,13 +18,29 @@ export class AnitopiaServerError extends Error {
    }
 }
 
+function checkServerMessageError(error: any) {
+   if (error.response && error.response.data) {
+      if (error.response.data.errors && error.response.data.errors[0]) return error.response.data.errors[0].msg
+
+      if (error.response.data.message) return error.response.data.message
+
+      return undefined
+   }
+}
+
 export function handleFetchError(error: any) {
+   let ServerMessageError = checkServerMessageError(error)
+
    //Client error - params/flood/ect.
    // How to use it? Just - throw new AnitopiaServerError("Not found", 404)
    // where first parameter is error message and the second is status code
    if (error instanceof AnitopiaServerError) {
       console.log('handleFetchError:', 1)
       showNotice(error.message, error.title, 'error')
+   }
+   //check message from server
+   else if (ServerMessageError !== undefined) {
+      showNotice(ServerMessageError, 'Помилка', 'error')
    }
    // Fetch failed error
    else if (error instanceof TypeError) {
