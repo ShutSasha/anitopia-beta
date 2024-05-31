4f42bf0f4ff9774390b43dff7c3bb76004345a79
Artem <temaprgod8@gmail.com>
2024-05-31 11:09:21 +0300

    [client] styles changes in search modal

--- client/src/widgets/header/types/seach-card.ts
+++ client/src/widgets/header/types/seach-card.ts
@@ -1,7 +1,12 @@
 export interface ISearchCard {
    id: number
    title: string
+   title_orig: string
+   shikimori_id: string
    material_data: {
+      anime_status: string
+      anime_kind: string
       poster_url: string
    }
+   year: string
 }
